34092a126c7fdbec7239e9158cabbd784e940570
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-04-27 18:44:11 +0200

    Update test-num-edges.js

---
+++ trees/test-num-edges.js
@@ -0,0 +1,25 @@
+const BinarySearchTree = require('./BinarySearchTree')
+
+const bst = new BinarySearchTree()
+
+
+bst.insert(23)
+bst.insert(13)
+bst.insert(7)
+bst.insert(15)
+bst.insert(54)
+bst.insert(46)
+bst.insert(77)
+bst.insert(42)
+bst.insert(75)
+bst.insert(18)
+bst.insert(6)
+bst.insert(8)
+//              23
+//           /      \
+//          13       54
+//         / \       / \
+//        7  15     46 77
+//       / \   \   /  /
+//      6   8  18 42 75
+console.log(bst.getNumEdges()) // 11 CONNESSIONI
